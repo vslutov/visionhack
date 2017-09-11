0a561249941a684b4ede67f5c7ef6ac715c37d66
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2017-09-11 15:00:18 +0300

    linker creates symbol links for files splitted by events directories

---
+++ linker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import subprocess
+
+events = [
+	"bridge", 
+	"city_entry", 
+	"city_exit", 
+	"road_bump",
+	"screen_wipers",
+	"zebra",
+]
+
+for ev in events:
+	shutil.rmtree(ev)
+
+for ev in events:
+	os.mkdir(ev)
+
+with open("train.txt", "r") as fin:
+	for filename, mask in [line.split() for line in fin.readlines()]:
+		for i in range(len(mask)):
+			if int(mask[i]):
+				subprocess.run(["ln", "-s", filename, events[i] + "/" + filename])
