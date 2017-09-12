0455ec439acdefe9d8dbe938072867826eafc1c6
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2017-09-12 13:04:32 +0300

    Fix generator

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ def custom_generator(path, batch_size):
     indices = list(range(len(y)))
     while True:
         shuffle(indices)
-        for b_start in range(0, len(indices), batch_size):
+        for b_start in range(0, len(indices) // batch_size * batch_size, batch_size):
             imgs = [
                 skimage.io.imread(os.path.join(path, imgs_files[indices[i]]))
                 for i in range(b_start, min(len(indices), b_start + batch_size))
@@ -15,4 +15,3 @@ def custom_generator(path, batch_size):
                 for i in range(b_start, min(len(indices), b_start + batch_size))
             ]
             yield imgs, sub_y
-	
